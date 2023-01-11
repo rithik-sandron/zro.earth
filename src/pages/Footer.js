7b18e79ab4553cc25043ea8cd082c312f1b7f31c
Rithik <rithik.ryuu@outlook.com>
2023-01-11 19:34:01 +0530

    init

---
+++ src/pages/Footer.js
@@ -0,0 +1,13 @@
+import React, { memo, useState } from 'react';
+import '../App.css';
+
+export default memo(function Footer() {
+
+    return (
+        <div className='footer'>
+            <span>
+                sample content used for display purpose.
+            </span>
+        </div>
+    )
+});
---
+++ src/pages/HighlightContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+export default function HighlightContent() {
+    return (
+        <div class="flex">
+            <div className='flex-item-1'>
+                <h1 className="headline">Learn Once, Write Anywhere</h1>
+                <div className='image' />
+            </div>
+        </div>
+    );
+}
