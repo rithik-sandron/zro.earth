406f142f895fbb79ff22c1787c3cbfd6c13ca510
Rithik <rithik.ryuu@outlook.com>
2023-01-24 19:09:47 +0530

    added main post background art

--- pages/HighlightContent.js
+++ pages/HighlightContent.js
@@ -3,9 +3,18 @@ import Link from 'next/link';
 export default function HighlightContent({ bg, fore, posts =
     [{ title: "", date: "", slug: "", author: { name: "", picture: "" }, coverImage: "", tagsWithColors: [{ tag: "", color: "" }] }]
 }) {
-    console.log('called')
+
+
     return (
-        <div className='head-content' style={{ backgroundColor: bg }}>
+        <div className='head-content' style={{
+            backgroundColor: bg,
+            background:
+            `conic-gradient(from 80deg at 2px 2px, #0000 75%, ${fore} 0),
+            linear-gradient(90deg, ${fore} 15%, ${bg} 0 58%, #0000 10%, ${bg} 0 90%, ${fore} 0),
+            conic-gradient(from 30deg at 70% 10%, ${fore} 65%, ${bg} 0)`,
+        backgroundSize: `64em 92em`
+
+        }}>
             {posts.map(post => {
                 return (
                     <div key={post.slug} className='head-content-container'>
@@ -16,6 +25,7 @@ export default function HighlightContent({ bg, fore, posts =
                                 style={{ border: `0.5px solid ${fore}`, boxShadow: `4px 10px 27px ${fore.substring(0, fore.length - 1)},0.5)` }} />
                         </Link>
                         <h1 className='post' style={{
+                            backgroundColor: bg,
                             color: fore
                         }}>{post.title}</h1>
                     </div>
