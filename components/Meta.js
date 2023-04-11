35a2e41bc16841d73da07312e7152a89349a4081
Rithik <81804302+rithik-sandron@users.noreply.github.com>
2023-04-11 21:12:50 +0530

    ui change

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 export default function Meta({ title = '', desc = '' }) {
     return (
         <Head>
-            <title>{title !== '' ? title : 'ZERロ'}</title>
+            <title>{title !== '' ? title : 'Zro'}</title>
             <meta property="og:site_name" content='ZERロ' />
             <meta property="og:url" content="https://zro.earth/" />
             <meta
@@ -12,8 +12,8 @@ export default function Meta({ title = '', desc = '' }) {
                 content={desc !== '' ? desc :
                     'A place where you can find articles related to Anime, Movies, TV shows, Game, Food, Restaurants etc..'}
             />
-            <meta property="og:title" content={title !== '' ? title : 'ZERロ'} />
-            <meta name="apple-mobile-web-app-title" content="Zero" />
+            <meta property="og:title" content={title !== '' ? title : 'Zro'} />
+            <meta name="apple-mobile-web-app-title" content="Zro" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <meta name="msapplication-TileColor" media="(prefers-color-scheme: light)" content={"#fff"} />
             <meta name="msapplication-TileColor" media="(prefers-color-scheme: dark)" content={"#050607"} />
