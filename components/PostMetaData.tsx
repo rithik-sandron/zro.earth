f72ab5183ffce07365781a120018ef1f9febeec3
Rithik <rithik.ryuu@outlook.com>
2023-11-11 11:51:16 +0000

    test commit

--- components/PostMetaData.tsx
+++ components/PostMetaData.tsx
@@ -3,14 +3,14 @@
 export default function PostMetaData({ post, doNeedGist }) {
 
     return (
-        <div style={{ backgroundColor: doNeedGist && post.color }}>
-        <section key={post.slug} className={doNeedGist ? 'container' : 'sub-container'}>
-            <span>
-                <li>{post.list}</li>
-            </span>
-            {doNeedGist ? <h1>{post.title}</h1> : <h3>{post.title}</h3>}
-            {doNeedGist && <p>{post.gist}</p>}
-        </section>
+        <div >
+            <section key={post.slug} className={doNeedGist ? 'container' : 'sub-container'}>
+                <span>
+                    <li>{post.list}</li>
+                </span>
+                {doNeedGist ? <h1>{post.title}</h1> : <h3>{post.title}</h3>}
+                {doNeedGist && <p>{post.gist}</p>}
+            </section>
         </div>
 
     );
