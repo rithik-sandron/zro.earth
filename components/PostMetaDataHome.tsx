07c3bad7d0dc0b467013fb5f8ed82a96f4fa1f4c
Rithik Sandron <81804302+rithik-sandron@users.noreply.github.com>
2024-08-02 12:59:53 +0100

    changes

--- components/PostMetaDataHome.tsx
+++ components/PostMetaDataHome.tsx
@@ -6,7 +6,7 @@ export default function PostMetaDataHome({ post, doNeedGist }) {
         <div style={{ backgroundColor: doNeedGist }}>
             <section key={post.slug} className={doNeedGist ? 'container' : 'sub-container'}>
                 {doNeedGist ? <h1>{post.metadata.title}</h1> : <h3>{post.metadata.title}</h3>}
-                <p style={{ textUnderlineOffset: "4px", marginTop: "1mm", fontSize: "3.2mm", color: "#555555" }}>{post.metadata.author}</p>
+                {/* <p style={{ textUnderlineOffset: "4px", marginTop: "1mm", fontSize: "3.2mm", color: "#555555" }}>{post.metadata.author}</p> */}
                 {doNeedGist && (<><br /><p>{post.metadata.description}</p></>)}
             </section>
         </div>
